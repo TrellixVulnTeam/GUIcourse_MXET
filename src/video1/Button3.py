e7af415e6f21517afd518b21fb733fd36d333d7f
sujaychandra <sujaychandraofficial@gmail.com>
2020-12-25 18:25:09 +0530

    second day 2

---
+++ src/video1/Button3.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root=Tk()
+
+def myClick():
+    showlabel=Label(root,text="you clicked me")
+    showlabel.pack()
+
+mybutton=Button(root,text="click me",state=DISABLED,fg="white",bg="black")
+
+mybutton2=Button(root,text="click me button",fg="blue",bg="#000000",padx=80,pady=40,command=myClick)
+
+
+mybutton.pack()
+mybutton2.pack()
+root.mainloop()
