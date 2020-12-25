46970461f3bedf91219dda313c2894ea7ce828a4
sujaychandra <sujaychandraofficial@gmail.com>
2020-12-25 12:20:58 +0530

    day one

--- src/video1/helloworldgui.py
+++ src/video1/helloworldgui.py
@@ -3,9 +3,10 @@ from tkinter import *
 # //creating the root window
 root = Tk()
 # creating a label widget
-helloworld_label = Label(root,text="Hello World")
+helloworld_label = Label(root,text="Hello Sujay")
 # packing in a window
 helloworld_label.pack()
 
 # looping the programing continously
 root.mainloop()
+
