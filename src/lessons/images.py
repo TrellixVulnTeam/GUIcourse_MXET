3bee3290a26c7b6d27b3eea03ec78cd973bb3745
sujaychandra <sujaychandraofficial@gmail.com>
2020-12-29 19:22:31 +0530

    #6 photo app

--- src/lessons/images.py
+++ src/lessons/images.py
@@ -6,14 +6,38 @@ root.title("Image tutorial")
 root.iconbitmap("../images/me.jpg")
 
 
-my_image=ImageTk.PhotoImage(Image.open("../images/me.jpg"))
-my_label=Label(image=my_image)
-my_label.pack()
+
+my_image1=ImageTk.PhotoImage(Image.open("../images/me.jpg"))
+my_image2=ImageTk.PhotoImage(Image.open("../images/breakfast.png"))
+my_image3=ImageTk.PhotoImage(Image.open("../images/breakfast2.png"))
+my_image4=ImageTk.PhotoImage(Image.open("../images/dinner.png"))
+my_image5=ImageTk.PhotoImage(Image.open("../images/lunch1.png"))
+my_image6=ImageTk.PhotoImage(Image.open("../images/snacks.png"))
+
+my_images=[my_image1,my_image2,my_image3,my_image4,my_image5,my_image6]
 
 
 
-btton_quit=Button(root,text="quit",command=root.quit,padx=50,pady=10)
-btton_quit.pack()
+
+
+my_label=Label(image=my_images[1])
+my_label.grid(row=0, column=0,columnspan=3)
+# my_label.pack()
+# my_label.grid(row=0, column=0, columnspan=3)
+def forward():
+    return
+
+def backs():
+    return
+
+
+
+button_back=Button(root,text="<<",command=lambda :backs())
+button_forward=Button(root,text=">>",command= lambda :forward())
+btton_quit=Button(root,text="Exit program",command=root.quit,padx=50,pady=10)
+btton_quit.grid(row=1,column=1)
+button_back.grid(row=1,column=0)
+button_forward.grid(row=1,column=2)
 
 
 root.mainloop()
