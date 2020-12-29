7062aa7f56a332e1da3402d518bf566eefe0e979
sujaychandra <sujaychandraofficial@gmail.com>
2020-12-29 18:09:09 +0530

    #6 practice image

--- src/lessons/images.py
+++ src/lessons/images.py
@@ -17,3 +17,20 @@ btton_quit.pack()
 
 
 root.mainloop()
+
+
+# from tkinter import *
+# from PIL import ImageTk,Image
+#
+# root=Tk()
+# root.title("image tutorial")
+# root.iconbitmap("../images/bell.png")
+#
+# my_image=ImageTk.PhotoImage(Image.open("../images/me.jpg"))
+# mylabel=Label(image=my_image)
+# mylabel.pack()
+#
+# button=Button(root,text="quit",command=root.quit)
+# button.pack()
+#
+# root.mainloop()
