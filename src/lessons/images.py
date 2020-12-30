8bf6c74bf9d46340d0d9c1cd008f4c10c622b278
sujaychandra <sujaychandraofficial@gmail.com>
2020-12-30 19:02:34 +0530

    #7 completed photo viewer app

--- src/lessons/images.py
+++ src/lessons/images.py
@@ -20,20 +20,43 @@ my_images=[my_image1,my_image2,my_image3,my_image4,my_image5,my_image6]
 
 
 
-my_label=Label(image=my_images[1])
+my_label=Label(image=my_images[0])
 my_label.grid(row=0, column=0,columnspan=3)
-# my_label.pack()
-# my_label.grid(row=0, column=0, columnspan=3)
+
+global counter
+counter = 0
+
 def forward():
-    return
+    global my_label
+    global counter
+    if  counter >= len(my_images)-1:
+        button_forward = Button(root, text=">>", command=forward,state=DISABLED)
+        button_forward.grid(row=1, column=2)
+    else:
+        counter += 1
+        my_label.grid_forget()
+        my_label = Label(image=my_images[counter])
+        my_label.grid(row=0, column=0, columnspan=3)
+
+
 
 def backs():
-    return
+    global my_label
+    global counter
+    if  counter <= 0:
+        button_back=Button(root,text="<<",command=backs,state=DISABLED)
+        button_back.grid(row=1, column=0)
+    else:
+        counter -=1
+        my_label.grid_forget()
+        my_label = Label(image=my_images[counter])
+        my_label.grid(row=0, column=0, columnspan=3)
 
 
 
-button_back=Button(root,text="<<",command=lambda :backs())
-button_forward=Button(root,text=">>",command= lambda :forward())
+
+button_back=Button(root,text="<<",command=backs)
+button_forward=Button(root,text=">>",command=forward)
 btton_quit=Button(root,text="Exit program",command=root.quit,padx=50,pady=10)
 btton_quit.grid(row=1,column=1)
 button_back.grid(row=1,column=0)
